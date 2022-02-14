d9e0a18472cdddc6e6e2365d8f0498ea0849b4b0
kg-phantom <ksurbayan@gmail.com>
2022-02-14 12:03:21 -0800

    protect dashboard and post editing routes

--- app/routes/api.py
+++ app/routes/api.py
@@ -2,6 +2,7 @@ import sys
 from flask import Blueprint, request, jsonify, session
 from app.models import User, Post, Comment, Vote
 from app.db import get_db
+from app.utils.auth import login_required
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 
@@ -61,6 +62,7 @@ def login():
     return jsonify(id = user.id)
 
 @bp.route('/comments', methods=['POST'])
+@login_required
 def comment():
     data = request.get_json()
     db = get_db()
@@ -84,6 +86,7 @@ def comment():
     return jsonify(id = newComment.id)
 
 @bp.route('/posts/upvote', methods=['PUT'])
+@login_required
 def upvote():
     data = request.get_json()
     db = get_db()
@@ -106,6 +109,7 @@ def upvote():
     return '', 204
 
 @bp.route('/posts', methods=['POST'])
+@login_required
 def create():
     data = request.get_json()
     db = get_db()
@@ -129,6 +133,7 @@ def create():
     return jsonify(id = newPost.id)
 
 @bp.route('/posts/<id>', methods=['PUT'])
+@login_required
 def update(id):
     data = request.get_json()
     db = get_db()
@@ -146,6 +151,7 @@ def update(id):
     return '', 204
 
 @bp.route('/posts/<id>', methods=['DELETE'])
+@login_required
 def delete(id):
     db = get_db()
 
